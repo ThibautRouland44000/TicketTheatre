c0f13fde8b704f817e88f7d7348488ffd539473a
Jérôme Oger <jerome.oger@etudiant.univ-lr.fr>
2026-05-04 16:46:38 +0200

    wip

--- payment-service/database/seeders/DatabaseSeeder.php
+++ payment-service/database/seeders/DatabaseSeeder.php
@@ -18,7 +18,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'last_name' => 'Test',
+            'first_name' => 'User',
             'email' => 'test@example.com',
         ]);
     }
